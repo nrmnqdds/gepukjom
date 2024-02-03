a315a32f91e9545fb86c782687bae8cde67f33d6
nrmnqdds <quddusnuriman@gmail.com>
2024-02-03 22:52:52 +0800

    :art: restructure

--- app/outlet/create/page.tsx
+++ app/outlet/create/page.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const CreateOutletPage = () => {
 	return (
-		<main className="bg-background w-full min-h-screen relative isolate p-24 sm:p-32 lg:p-40">
+		<>
 			<h1 className="text-5xl font-bold mb-5 sm:mb-10">New Outlet</h1>
 			<CreateForm />
-		</main>
+		</>
 	);
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,14 @@ import Spotlight from "@/components/ui/spotlight";
 
 const Home = () => {
 	return (
-		<main className="bg-background relative pt-14 min-h-screen">
-			<div className="py-24 sm:py-32 lg:pb-40">
-				<Spotlight
-					className="-top-40 left-0 md:left-60 md:-top-20"
-					fill="white"
-				/>
-				<div className="mx-auto max-w-7xl px-6 lg:px-8">
-					<Intro />
-					{/* <div className="mt-16 flow-root sm:mt-24">
+		<>
+			<Spotlight
+				className="-top-40 left-0 md:left-60 md:-top-20"
+				fill="white"
+			/>
+			<div className="mx-auto max-w-7xl px-6 lg:px-8">
+				<Intro />
+				{/* <div className="mt-16 flow-root sm:mt-24">
 						<div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
 							<img
 								src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
@@ -22,9 +21,8 @@ const Home = () => {
 							/>
 						</div>
 					</div> */}
-				</div>
 			</div>
-		</main>
+		</>
 	);
 };
 
